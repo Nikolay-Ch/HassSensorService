34c05fe879fcc29d2d50077860a98dc3231f196d
Nikolay Chapkin <nic-ch-work@yandex.ru>
2020-11-04 23:15:21 +0300

    Set ClientId for MQTT-broker to static MachineId

--- AprilBeaconsHomeAssistantIntegrationService/MqttConfiguration.cs
+++ AprilBeaconsHomeAssistantIntegrationService/MqttConfiguration.cs
@@ -1,11 +1,18 @@
-﻿using MQTTnet.Protocol;
-using System;
+﻿using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+using MQTTnet.Protocol;
+using System.Security.Cryptography;
 
 namespace AprilBeaconsHomeAssistantIntegrationService
 {
     public class MqttConfiguration
     {
-        public string ClientId { get; set; } = Guid.NewGuid().ToString();
+        // create ClientId: GUID in windows systems, and /etc/machine-id in linux
+        public string ClientId { get; set; } = new DeviceIdBuilder()
+            .AddOSInstallationID()
+            .UseFormatter(new StringDeviceIdFormatter(new PlainTextDeviceIdComponentEncoder()))
+            .ToString();
         public string MqttUri { get; set; }
         public string MqttUser { get; set; }
         public string MqttUserPassword { get; set; }
