621ece56f96437be862c3a012d1bae40d61e311d
Nikolay-Ch <nic-ch-work@yandex.ru>
2024-05-10 12:13:49 +0300

    Set Modbus timeout default value to 30s

--- HassDeviceWorkers/ModBus/ModbusGatewayConfiguration.cs
+++ HassDeviceWorkers/ModBus/ModbusGatewayConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public record class ModbusGatewayConfiguration
     {
-        public required int SendTimeout { get; set; }
+        public required int SendTimeout { get; set; } = 30000;
         public required string GatewayAddress { get; set; }
         public required int GatewayPort { get; set; }
     }
